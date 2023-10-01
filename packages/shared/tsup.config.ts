e3bab03c17ebe2644185ae98d43e908e3804e55e
Riccardo Perra <perrariccardo0@gmail.com>
2023-10-01 10:31:57 +0200

    update libraries and fix dependencies

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "tsup";
 import pkq from "./package.json";
 
 const entryPaths = Object.keys(pkq.exports).map((path) =>
-  path === "." ? "src/index.ts" : `src/${path.substring(2)}.ts`
+  path === "." ? "src/index.ts" : `src/${path.substring(2)}.ts`,
 );
 
 export default defineConfig((config) => ({
   clean: config.watch ? false : true,
   dts: { entry: entryPaths },
-  format: ["cjs", "esm"],
+  format: ["esm"],
   entryPoints: entryPaths,
+  sourcemap: true,
   external: ["msw"],
 }));
